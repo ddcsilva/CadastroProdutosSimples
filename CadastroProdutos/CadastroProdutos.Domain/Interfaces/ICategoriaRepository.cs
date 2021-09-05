0ed8dc5102b0bc792dbec6f9e24753e7c8d22578
Danilo Silva <danilo.silva@msn.com>
2021-09-05 20:07:19 -0300

    Criação das Interfaces para padrão Repository

---
+++ CadastroProdutos/CadastroProdutos.Domain/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroProdutos.Domain
+{
+    public interface ICategoriaRepository
+    {
+        Task<IEnumerable<Categoria>> ListarAsync();
+        Task<Categoria> ConsultarAsync(int? id);
+        Task<Categoria> CreateAsync(Categoria categoria);
+        Task<Categoria> UpdateAsync(Categoria categoria);
+        Task<Categoria> RemoveAsync(Categoria categoria);
+    }
+}
---
+++ CadastroProdutos/CadastroProdutos.Domain/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CadastroProdutos.Domain
+{
+    public interface IProdutoRepository
+    {
+        Task<IEnumerable<Produto>> ListarAsync();
+        Task<Produto> ConsultarAsync(int? id);
+        Task<Produto> ConsultarProdutoPorCategoriaAsync(int? id);
+        Task<Produto> CreateAsync(Produto produto);
+        Task<Produto> UpdateAsync(Produto produto);
+        Task<Produto> RemoveAsync(Produto produto);
+    }
+}
